27717b27e6a615357ea604ec667b4202a9f79682
Toby Flemming <tobyf93@hotmail.com>
2016-10-25 13:02:08 +1100

    Initial commit.  Tests written.

---
+++ spec/trafficLightsSpec.js
@@ -0,0 +1,49 @@
+const nextState = require('../lib/trafficLights');
+
+describe('NS green-red transition', () => {
+  let lights = [
+    {
+      name: 'NS',
+      colour: 'GREEN'
+    },
+    {
+      name: 'EW',
+      colour: 'RED'
+    }
+  ];
+
+  it('must turn orange', () => {
+    lights = nextState(lights);
+    expect(lights[0].colour).toBe('ORANGE');
+  });
+  it('must turn red, with other lights turning green', () => {
+    lights = nextState(lights);
+    lights = nextState(lights);
+    expect(lights[0].colour).toBe('RED');
+    expect(lights[1].colour).toBe('GREEN');
+  });
+});
+
+describe('EW green-red transition', () => {
+  let lights = [
+    {
+      name: 'NS',
+      colour: 'RED'
+    },
+    {
+      name: 'EW',
+      colour: 'GREEN'
+    }
+  ];
+
+  it('must turn orange', () => {
+    lights = nextState(lights);
+    expect(lights[1].colour).toBe('ORANGE');
+  });
+  it('must turn red, with other lights turning green', () => {
+    lights = nextState(lights);
+    lights = nextState(lights);
+    expect(lights[0].colour).toBe('GREEN');
+    expect(lights[1].colour).toBe('RED');
+  });
+});
